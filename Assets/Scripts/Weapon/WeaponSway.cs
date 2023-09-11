68d6a4c95c1a95d4c988004dbd3fcaac99ed55eb
Džiugas Pumputis <dziugaspumputis@gmail.com>
2023-09-11 18:26:39 +0300

    Added weapon sway while moving
    now weapon sways side to side while walking and even more while sprinting :)

--- Assets/Scripts/Weapon/WeaponSway.cs
+++ Assets/Scripts/Weapon/WeaponSway.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 
 public class WeaponSway : MonoBehaviour
 {
-    [Header("Sway")] 
+    [Header("Sway")]
     [SerializeField] private float smooth;
     [SerializeField] private float swayMultiplier;
+    [SerializeField] private float walkSwayAmount;
+    [SerializeField] private float walkSwaySpeed;
+    [SerializeField] private float sprintSwayAmount;
+    [SerializeField] private float sprintSwaySpeed;
+
+    private float swayTime = 0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        GameObject playerObject = GameObject.FindWithTag("Player");
+        if(playerObject != null)
+        {
+            rb = playerObject.GetComponent<Rigidbody>();
+        }
+        else
+        {
+            Debug.LogError("Player GameObject not found with the 'Player' tag.");
+        }
+    }
 
     private void Update()
     {
@@ -20,5 +41,30 @@ public class WeaponSway : MonoBehaviour
         Quaternion targetRotation = rotationX * rotationY;
 
         transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, smooth * Time.deltaTime);
+
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+
+        float playerSpeed = rb.velocity.magnitude;
+
+        if (horizontalInput != 0f || verticalInput != 0f && playerSpeed < 8f)
+        {
+            swayTime += Time.deltaTime * walkSwaySpeed;
+            float swayOffset = Mathf.Sin(swayTime) * walkSwayAmount;
+
+            Quaternion swayRotation = Quaternion.Euler(0f, 0f, swayOffset);
+
+            transform.localRotation = transform.localRotation * swayRotation;
+        }
+        Debug.Log(playerSpeed);
+        if (horizontalInput != 0f || verticalInput != 0f && playerSpeed > 8f)
+        {
+            swayTime += Time.deltaTime * sprintSwaySpeed;
+            float swayOffset = Mathf.Sin(swayTime) * sprintSwayAmount;
+
+            Quaternion swayRotation = Quaternion.Euler(0f, 0f, swayOffset);
+
+            transform.localRotation = transform.localRotation * swayRotation;
+        }
     }
 }
